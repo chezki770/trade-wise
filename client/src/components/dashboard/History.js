ceb010072a104a6f13ae1af5df775d01023ed75d
Yehezkel Mrkovitch <151782660+chezki770@users.noreply.github.com>
2024-11-27 14:46:38 +0200

    Update History.js

--- client/src/components/dashboard/History.js
+++ client/src/components/dashboard/History.js
@@ -1,81 +1,176 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-
-function Record({ date, t_type, symbol, shares, price }) {
-    let sign = "";
-    if(t_type === "BUY") sign = '-';
-    else sign = '+';
-    return (
-    <li>{date} - {t_type} {symbol} - {shares} shares ({sign}${price*shares})</li>
-   );
-}
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 class History extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            transactions: this.props.auth.user.transactions,
-            errors: {}
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTimeframe: '1M', // '1D', '1W', '1M', '3M', '1Y', 'ALL'
+      chartData: [],
+    };
+  }
 
-    componentDidMount() {
-        this.setState({
-            transactions: this.props.auth.user.transactions
-        });
-    }
+  componentDidMount() {
+    this.generateChartData();
+  }
 
-    render() {
-        console.log("STATE: " + this.state);
-        console.log("PROPS: " + this.props.auth);
+  generateChartData = () => {
+    // In a real app, this would fetch historical data from an API
+    // For demo, we'll generate sample data
+    const { transactions = [] } = this.props.auth.user;
+    
+    // Sort transactions by date
+    const sortedTransactions = [...transactions].sort((a, b) => 
+      new Date(a.date) - new Date(b.date)
+    );
 
-        let transactions = undefined;
-        if(this.state.transactions != null) {
-            transactions = this.state.transactions.map((record, ii) => {
-                console.log(record);
-                let date = new Date(record.date).toDateString();
-                return(
-                    <Record
-                        date={date}
-                        t_type={record.transaction_type}
-                        symbol={record.symbol}
-                        shares={record.stock_quantity}
-                        price={record.stock_price}
-                        key={ii}
-                    />
-                );
-            });
-        } else { 
-            transactions = "No transactions made yet.";
-        }
-        
-        return(
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
-                <div className="row">
-                    <div className="col s12 center-align">
-                        <h3>Transaction Record</h3>
-                        <ul>
-                            {transactions}
-                        </ul>
+    // Generate cumulative portfolio value over time
+    let portfolioValue = 0;
+    const chartData = sortedTransactions.map(transaction => {
+      const amount = transaction.stock_price * transaction.stock_quantity;
+      portfolioValue += transaction.transaction_type === "BUY" ? -amount : amount;
+      
+      return {
+        date: new Date(transaction.date).toLocaleDateString(),
+        value: portfolioValue,
+        type: transaction.transaction_type,
+        symbol: transaction.symbol,
+      };
+    });
+
+    this.setState({ chartData });
+  };
+
+  render() {
+    const { user } = this.props.auth;
+    const { chartData } = this.state;
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Trading History</b>
+            </h4>
+
+            {/* Portfolio Performance Graph */}
+            <div className="row">
+              <div className="col s12">
+                <div className="card">
+                  <div className="card-content">
+                    <span className="card-title">Portfolio Performance</span>
+                    <div style={{ width: '100%', height: 400 }}>
+                      <ResponsiveContainer>
+                        <LineChart data={chartData}>
+                          <CartesianGrid strokeDasharray="3 3" />
+                          <XAxis dataKey="date" />
+                          <YAxis />
+                          <Tooltip />
+                          <Legend />
+                          <Line 
+                            type="monotone" 
+                            dataKey="value" 
+                            stroke="#8884d8" 
+                            name="Portfolio Value"
+                          />
+                        </LineChart>
+                      </ResponsiveContainer>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        );
-    }
+
+            {/* Transaction History Table */}
+            <div className="row">
+              <div className="col s12">
+                <div className="card">
+                  <div className="card-content">
+                    <span className="card-title">Transaction History</span>
+                    <table className="striped">
+                      <thead>
+                        <tr>
+                          <th>Date</th>
+                          <th>Type</th>
+                          <th>Symbol</th>
+                          <th>Shares</th>
+                          <th>Price</th>
+                          <th>Total</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {user.transactions?.slice().reverse().map((transaction, index) => {
+                          const total = transaction.stock_price * transaction.stock_quantity;
+                          return (
+                            <tr key={index}>
+                              <td>{new Date(transaction.date).toLocaleString()}</td>
+                              <td>
+                                <span className={transaction.transaction_type === "BUY" ? "green-text" : "red-text"}>
+                                  {transaction.transaction_type}
+                                </span>
+                              </td>
+                              <td>{transaction.symbol}</td>
+                              <td>{transaction.stock_quantity}</td>
+                              <td>${transaction.stock_price.toFixed(2)}</td>
+                              <td className={transaction.transaction_type === "BUY" ? "red-text" : "green-text"}>
+                                {transaction.transaction_type === "BUY" ? "-" : "+"}${total.toFixed(2)}
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Summary Statistics */}
+            <div className="row">
+              <div className="col s12 m4">
+                <div className="card blue-grey darken-1">
+                  <div className="card-content white-text">
+                    <span className="card-title">Total Trades</span>
+                    <h4>{user.transactions?.length || 0}</h4>
+                  </div>
+                </div>
+              </div>
+              <div className="col s12 m4">
+                <div className="card blue-grey darken-1">
+                  <div className="card-content white-text">
+                    <span className="card-title">Buy Orders</span>
+                    <h4>
+                      {user.transactions?.filter(t => t.transaction_type === "BUY").length || 0}
+                    </h4>
+                  </div>
+                </div>
+              </div>
+              <div className="col s12 m4">
+                <div className="card blue-grey darken-1">
+                  <div className="card-content white-text">
+                    <span className="card-title">Sell Orders</span>
+                    <h4>
+                      {user.transactions?.filter(t => t.transaction_type === "SELL").length || 0}
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 History.propTypes = {
-    auth: PropTypes.object.isRequired,
-    user: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    stock: state.stock
+  auth: state.auth
 });
 
-export default connect(
-    mapStateToProps,
-    {}
-)(History);
+export default connect(mapStateToProps)(History);
